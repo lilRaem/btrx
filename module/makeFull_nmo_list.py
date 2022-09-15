4ce5597b4d4c9246454136d6a42fc3f27678e0ea
raemi <maxim3053@gmail.com>
2022-09-15 17:59:27 +0300

    make menu for search SPO price

--- module/makeFull_nmo_list.py
+++ module/makeFull_nmo_list.py
@@ -1,47 +1,114 @@
+from datetime import date
 import json
 import os
-from unicodedata import name
 
 
 
 
 def loadJsonfile():
 	with open(f'{os.getcwd()}\\data\\json\\program_СПО.json','r',encoding='utf-8') as file:
-		programSPO = json.load(file)
-	temper_json = {}
+		programNMP = json.load(file)
+	today = date.today()
+	cur_date = today.strftime("%d.%m.%Y")
+	filename = f'{cur_date}_file.json'
+	path = os.getcwd() + "\\data\\json\\btrx_data"
+	with open(f"{path}\{filename}",'r',encoding='utf-8') as file:
+		program = json.load(file)
 	temper_list = []
-	js=[]
-	count = 0
-	tmp = {}
-	i = 0
-	sp = 0
-	for k in programSPO:
-		spec_c = 0
-		tit_spec = k['title_spec']
-		temper_list.append({
-			"title_spec": programSPO[i]['title_spec'],
-			"title_program": programSPO[i]['title_program'],
-			"hour": programSPO[i]['hour'],
-			"date": programSPO[i]['date'],
-			'linkNmo': programSPO[i]['linkNmo']
-			}
-		)
+	temper_json = {}
+	prog_id = ''
+	prog_name = ''
+	prog_price = ''
+	count_nmp = 0
+	for i,prognmp in enumerate(programNMP):
 
-		if  programSPO[spec_c]['title_spec'] == tit_spec :
-			if tit_spec == programSPO[sp]['title_spec']:
-				temper_json = {
-					"title_spec": tit_spec,
-					"programs":temper_list
-				}
-				spec_c = spec_c + 1
-				sp = sp + 1
-			js.append(temper_json)
-			with open(f'{os.getcwd()}\\data\\json\\tempetOPT_NMO.json','w',encoding='utf-8') as file:
-				json.dump(js,file,indent=4,ensure_ascii=False)
-		i = i + 1
-		count = count + 1
+		for a,prog in enumerate(program):
+
+			prog_id = prog['ID']
+			prog_name = prog['NAME']
+			prog_price = prog['PRICE']
+			# print(prognmp['title_program'])
+			if prognmp['title_program'] == prog_name:
+					temper_list.append({
+						'id': prog_id,
+						"title_spec": prognmp['title_spec'].replace('\n',' '),
+						"title_program": prognmp['title_program'],
+						"price": prog_price.replace('.00',''),
+						"hour": prognmp['hour'],
+						"date": prognmp['date'],
+						'linkNmo': prognmp['linkNmo']
+						}
+					)
+
+
+	# for i,prog in enumerate(programNMP):
+	# 	tit_spec = prog['title_spec']
+	# 	for it,progt in enumerate(programNMP):
+	# 		tit_spect = progt['title_spec']
+
+
+	# 		if  tit_spec == tit_spect:
+	# 			temper_list.append({
+	# 				"title_spec": progt['title_spec'],
+	# 				"title_program": prog['title_program'],
+	# 				"hour": prog['hour'],
+	# 				"date": prog['date'],
+	# 				'linkNmo': prog['linkNmo']
+	# 				}
+	# 			)
+
+	# 			temper_json = {
+	# 				"title_spec": tit_spec,
+	# 				"programs":temper_list[sp]
+	# 			}
+	# 			sp = sp + 1
+	# 		js.append(temper_json)
+
+	with open(f'{os.getcwd()}\\data\\json\\tempetOPT_NMO.json','w',encoding='utf-8') as file:
+		json.dump(temper_list,file,indent=4,ensure_ascii=False)
+
+def funcfind(program,name,hour):
+	name = input('name: ')
+	hour = input('hour: ')
+	for i,data in enumerate(program):
+		if data['PROPERTY_213'] != None:
+			if name.lower() in data['NAME'].lower() and hour in data['PROPERTY_213']['value']:
+				print(f'\nby name and hour: {data["ID"]}')
+				print(f"{data['NAME']} {data['PRICE'].replace('.00','')}\n")
+			elif name.lower() in data['NAME'].lower():
+				print('by name: ',data['ID'])
+				print(data['NAME'],data['PRICE'])
+va = 0
+def funcmenu():
+
+	va = input('Выбор: ')
+
+def find():
+	today = date.today()
+	cur_date = today.strftime("%d.%m.%Y")
+	filename = f'{cur_date}_file.json'
+	path = os.getcwd() + "\\data\\json\\btrx_data"
+	with open(f"{path}\{filename}",'r',encoding='utf-8') as file:
+		program = json.load(file)
+	# with open(f'{os.getcwd()}\\data\\json\\program_СПО.json','r',encoding='utf-8') as file:
+	# 	f = json.load(file)
+	# tid = input('id: ')
+	# name = input('name: ')
+	# hour = input('hour: ')
+	btn = True
+
+	while True:
+
+		if va != 2:
+			funcmenu()
+		if va == 1:
+			funcfind(program)
+
+
+
+
 def main():
 	loadJsonfile()
-
+	find()
 if __name__ == "__main__":
 	main()
