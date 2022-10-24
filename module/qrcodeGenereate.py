689d48b1c3f37ffbb811533f0e02e8af123c284f
raemi <maxim3053@gmail.com>
2022-10-25 00:23:09 +0300

    add load image

--- module/qrcodeGenereate.py
+++ module/qrcodeGenereate.py
@@ -1,10 +1,23 @@
+import base64
+import json
 import requests
-
+import PIL.Image
 url = "https://api.qrcode-monkey.com//qr/custom"
+data = "https://web-apkipp.ru/newform/assets/docs/NMO/%D0%90%D0%9F%D0%9A%20%D0%B8%20%D0%9F%D0%9F%20%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D1%8B%20%D0%9F%D0%9A%20%D0%9D%D0%9C%D0%9E%20%2021.10.2022.pdf"
 
-with open('logo.png','r') as file:
-	print(file.flush)
+s = None
 
+image = PIL.Image.open("logo.png")
+bytes = image.tobytes()
+mystr = base64.b64encode(bytes).decode('utf-8')
+payload_img = {
+	"download": mystr,
+	"data": "hELL",
+}
+headers = {
+	"content-type": "application/json"
+}
+response = requests.request("POST", url, json=payload_img, headers=headers)
 payload = {
 	"config":{
 	"body": "circular",
@@ -28,22 +41,21 @@ payload = {
 	"gradientColor2": "",
 	"gradientType": "linear",
 	"gradientOnEyes": False,
-	"logo": "",
+	"logo": 'a8077b772dea1044706a2646bf7056b07cfad66a.png',
 	"logoMode": "default"
 	},
-	"data": "https://web-apkipp.ru/newform/assets/docs/NMO/%D0%90%D0%9F%D0%9A%20%D0%B8%20%D0%9F%D0%9F%20%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D1%8B%20%D0%9F%D0%9A%20%D0%9D%D0%9C%D0%9E%20%2021.10.2022.pdf",
+	"data":response.text,
+
 	"file": "svg",
 	"size": 400,
 }
 headers = {
-	"content-type": "application/json",
-	"X-RapidAPI-Key": "1b39f61d32msh113bae3507a8747p13948ejsn82e29bf847a1",
-	"X-RapidAPI-Host": "qrcode-monkey.p.rapidapi.com"
+	"content-type": "application/json"
 }
 
-# response = requests.request("POST", url, json=payload, headers=headers)
+response_Fin = requests.request("POST", url, json=payload_img, headers=headers)
+if response_Fin.status_code == 200:
+	print(f'QR-code generated!\n{response_Fin.text}')
 
-# print(response.text)
-
-# with open('file.svg', 'w') as f:
-# 	f.write(response.text)
+with open('file.svg', 'w') as f:
+	f.write(response_Fin.text)
