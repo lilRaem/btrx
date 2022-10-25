3a7f46a1ec37ca335191bcc909a0fb1fe13445af
raemi <maxim3053@gmail.com>
2022-10-26 00:51:08 +0300

    fix return data

--- module/html_pars/parserhtml.py
+++ module/html_pars/parserhtml.py
@@ -3,13 +3,21 @@ from typing import Optional
 from colorama import Fore,Back,Style
 from pydantic import BaseModel, StrictStr
 from bs4 import BeautifulSoup
-try:
-	from config import FinalData
-except:
-	from module.config import FinalData
 import requests
 import os, json
 
+from typing import Optional
+from pydantic import BaseModel, StrictStr
+
+class FinalData(BaseModel):
+	id: Optional[StrictStr] = None
+	spec: Optional[StrictStr] = None
+	name: Optional[StrictStr] = None
+	price: Optional[StrictStr] = None
+	hour: Optional[StrictStr] = None
+	nmoSpec: Optional[StrictStr] = None
+	linkNmo: Optional[StrictStr] = None
+	url: Optional[StrictStr] = None
 
 def bs4pars():
 	with open(f"{os.getcwd()}/module/html/templates/source/pp_spo.html",'r',encoding='utf-8') as f:
@@ -100,33 +108,37 @@ def parseSiteUrl(parseurl="https://apkipp.ru/katalog/zdravoohranenie-nemeditsins
     'Access-Control-Max-Age': '3600',
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/51.0'
     }
+	final_data = FinalData()
+	site_data_list = []
 	req = requests.get(parseurl, headers,timeout=None)
 	soup = BeautifulSoup(req.content,'lxml')
 
-	final_data = FinalData()
+
 
 	final_data.name = soup.find('h1','main-title').text
 	final_data.hour = soup.find('div','items-box-block__element-type-item').findChildren('span')[0].text.replace('часов', '').replace('часа', '').strip()
 	final_data.price = soup.find('div','course-info-block__action-buy-price').findChildren('span')[0].text.strip()
 	final_data.url = parseurl
 
-	site_data_list = []
 
 	count = 0
 	if final_data.price == price:
-		site_data_list.append(final_data.json(encoder='utf-8',ensure_ascii=False))
+		site_data_list.append(json.loads(final_data.json(ensure_ascii=False)))
+		print(site_data_list[0]['price'])
 	else:
+		final_data.hour = None
 		final_data.price = None
 		final_data.url = None
-		site_data_list.append(final_data.json(encoder='utf-8',ensure_ascii=False))
+		site_data_list.append(json.loads(final_data.json(ensure_ascii=False)))
 	for data in site_data_list:
 		count = count + 1
 	if count == 1:
 		print(Fore.MAGENTA+f'(parserhtml.py|parseSiteUrl(): count = 1) Search time: {round(time()-start,2)} sec' + Fore.RESET)
-		return site_data_list[0]
+		return json.loads(final_data.json(ensure_ascii=False))
 	else:
 		print('fail parse too many programs')
 		print(Fore.MAGENTA+f'(parserhtml.py|parseSiteUrl(): count > 1) Search time: {round(time()-start,2)} sec' + Fore.RESET)
+		print(site_data_list[0])
 		return site_data_list
 
 def main():
