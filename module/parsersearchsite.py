57f81cabafbf84281efa4d48657293d4d2d68f90
raemi <maxim3053@gmail.com>
2022-10-26 00:51:43 +0300

    fix return data

--- module/parsersearchsite.py
+++ module/parsersearchsite.py
@@ -16,8 +16,6 @@ except:
 
 link = 'https://apkipp.ru'
 
-#FIXME возвращает неверную информацию 0 и list с 9ю элементами
-
 def searchInSite(search_key='Онкология'):
 	'''Поиск на сайте по слову и сохраяет результат в data/json/site_search.json'''
 
@@ -53,54 +51,54 @@ def getProgramUrl(search_key='Онкология',price='6400'):
 		local_data = json.load(f)
 	for k, v in enumerate(local_data):
 		final_data.name = v['name']
+		final_data.price = price
 		if search_key.lower() in final_data.name.lower():
 			final_data.url = v['url']
-
 			main_url = link + final_data.url
 			pSiteUrl = phtml.parseSiteUrl(main_url,price)
 			if pSiteUrl != None:
 				find_url_list.append(pSiteUrl)
 				# print(Fore.GREEN+f'{pSiteUrl}'+Style.RESET_ALL)
 			else:
-				# find_url_list.append(pSiteUrl)
+				find_url_list.append(pSiteUrl)
 				print(f"\
 				{Fore.LIGHTWHITE_EX+final_data.url+' '+Fore.RESET+Fore.LIGHTBLACK_EX}\n\
 				[{str(k+1)}] {link}{final_data.url}"+Fore.RESET)
+
 	if find_url_list != []:
 		count_find_url = 0
-		for data in find_url_list:
-			count_find_url = count_find_url + 1
+		count_find_by_word = 0
 		if count_find_url == 1:
 			print(Fore.MAGENTA+f'(parsersearchsite.py|getProgramUrl(): count_find_url = 1) Search time: {round(time()-start,2)} sec' + Fore.RESET)
-			return find_url_list[0]
 		else:
-			print(f'Найдено {count_find_url} страниц с названием: {search_key}')
-			el_c = 0
-			for data in find_url_list:
-				json_data = json.loads(data)
-				print("\n"+Fore.CYAN+f'{json_data}'+Fore.RESET)
-				if search_key.lower() in json_data['name'].lower():
-					if price == json_data['price']:
-						el_c = el_c + 1
+			for d in find_url_list:
+				count_find_by_word = count_find_by_word + 1
+			print(f'Найдено {count_find_by_word} страниц с названием: {search_key}')
+			for i,data in enumerate(find_url_list):
+				if search_key.lower() in data['name'].lower():
+					if final_data.price == data['price']:
+						print("\n"+Fore.CYAN+f'{find_url_list[i]}'+Fore.RESET)
+						count_find_url = count_find_url + 1
 				else:
-					print(f'Найденно {el_c} страниц:\n{find_url_list}')
-					return json.dumps(find_url_list)
+					print(f'Найденно {count_find_url} страниц:\n{find_url_list}')
+					return find_url_list
 	else:
 		print('Url not found')
 
-	if el_c == 1:
+	if count_find_url == 1:
 		for i,data in enumerate(find_url_list):
-				json_data = json.loads(data)
-				if search_key.lower() in json_data['name'].lower():
-					if price == json_data['price']:
-						print(f'Найдена {el_c} страница:\n{json_data["name"]}|{json_data["hour"]}|{json_data["price"]}')
+
+				if search_key.lower() in data['name'].lower():
+
+					if price == data['price']:
+						print(f'Найдена {count_find_url} страница:\n{data["name"]}|{data["hour"]}|{data["price"]}')
 						return find_url_list[i]
 	else:
-		print(f'Найдено {el_c} страниц:\n{find_url_list}')
-		if el_c == 0:
-			return 0
+		print(f'Найдено {count_find_url} страниц:\n{find_url_list}')
+		if count_find_url == 0:
+			return find_url_list
 		else:
-			return json.dumps(find_url_list)
+			return json.loads(find_url_list)
 if __name__ == "__main__":
 
 	searchInSite('Онкология')
