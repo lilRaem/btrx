3ff8ccf7f227065749d83fb6efeebc558651cda2
raemi <maxim3053@gmail.com>
2022-10-27 01:50:20 +0300

    upd

--- module/qrcodeGenereate.py
+++ module/qrcodeGenereate.py
@@ -1,23 +1,9 @@
 import base64
 import json
 import requests
-import PIL.Image
 url = "https://api.qrcode-monkey.com//qr/custom"
 data = "https://web-apkipp.ru/newform/assets/docs/NMO/%D0%90%D0%9F%D0%9A%20%D0%B8%20%D0%9F%D0%9F%20%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D1%8B%20%D0%9F%D0%9A%20%D0%9D%D0%9C%D0%9E%20%2021.10.2022.pdf"
 
-s = None
-
-image = PIL.Image.open("logo.png")
-bytes = image.tobytes()
-mystr = base64.b64encode(bytes).decode('utf-8')
-payload_img = {
-	"download": mystr,
-	"data": "hELL",
-}
-headers = {
-	"content-type": "application/json"
-}
-response = requests.request("POST", url, json=payload_img, headers=headers)
 payload = {
 	"config":{
 	"body": "circular",
@@ -44,18 +30,18 @@ payload = {
 	"logo": 'a8077b772dea1044706a2646bf7056b07cfad66a.png',
 	"logoMode": "default"
 	},
-	"data":response.text,
+	"data":data,
 
 	"file": "svg",
-	"size": 400,
+	"size": 100,
 }
 headers = {
 	"content-type": "application/json"
 }
 
-response_Fin = requests.request("POST", url, json=payload_img, headers=headers)
+response_Fin = requests.request("POST", url, json=payload, headers=headers)
 if response_Fin.status_code == 200:
-	print(f'QR-code generated!\n{response_Fin.text}')
+	print(f'QR-code generated!')
 
 with open('file.svg', 'w') as f:
 	f.write(response_Fin.text)
