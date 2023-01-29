5a99abd6d4cae1c80c924a73cbf4dabecdcbac24
raemi <maxim3053@gmail.com>
2023-01-29 16:21:16 +0300

    turn to class

---
+++ docs/ProjectLibs.md
@@ -0,0 +1,59 @@
+## fast_bitrix24
+
+[fast_bitrix24](https://github.com/leshchenko1979/fast_bitrix24 "fast_bitrix24") - API wrapper для Питона для быстрого получения данных от Битрикс24 через REST API.
+
+#### Как используется в проекте?
+
+##### Расположение файла методов:
+
+[module/btrx.py](module/btrx.py)
+
+##### Методы:
+
+###### save_to_json(datalist, filename, path):
+
+> Сохраняет `datalist` по пути `path` c именем `filename`:`/data/json/btrx_data/XX.XX.XX_file.json`
+> возвращает преобразованный в json **list(`datalist`)**
+
+###### load_from_jsonFile(filename, path):
+
+> Загружает файл по имени `filename` и путь `path`: `/data/json/btrx_data/XX.XX.XX_file.json` 
+> возвращает преобразованный в  json **list(`data_from_file`)**
+
+###### get_product_list(btrx):
+
+> Наследует класс `Bitrix` с webhook'ом
+> Загружает список товаров с полями: 
+> <span style='color: magenta;'>id</span>, <span style='color: yellow;'>name</span>, <span style='color: lightgreen;'>price</span>, <span style='color: lightblue;'>hour</span> 
+> возвращает **list(products)**
+
+###### get_users_with_innerPhone(btrx):
+
+> Наследует класс `Bitrix` с webhook'ом
+> Загружает список пользователей со всеми полями
+> возвращает **list(users)**
+
+###### set_product_price(btrx,id,price):
+
+> Наследует класс `Bitrix` с webhook'ом
+> обновлят поле `price` по `id`
+
+###### get_all_data(data):
+
+> Принимает list `data` из `get_product_list(btrx)` и преобразует данные в  json-формат
+> возвращает **list(datalist)**
+
+---
+
+## Pydantic
+
+[Pydantic](https://pydantic-docs.helpmanual.io/ "Pydantic") - работа с JSON данными
+
+---
+
+## Colorama
+
+[Colorama](https://github.com/tartley/colorama) - Оформление текста в терминале
+
+
+4. [Requests](https://github.com/psf/requests) - Веб запросы и отправка
--- tests/btrx_test.py
+++ tests/btrx_test.py
@@ -1,74 +1,22 @@
 from unittest import TestCase, main
-import btrx
-from main import *
-
+from module.btrx import Btrx
 
 class BtrxTest(TestCase):
 
-	def test_webhook(self):
-		self.test_webhook_var = 'https://b24-vejk6x.bitrix24.ru/rest/1/di28gta836z3xn50/'
-		self.assertEqual(type(btrx.webhook), type(self.test_webhook_var))
-		return self.test_webhook_var
-
-	def test_btrx(self):
-		test_btrx_var = btrx.Bitrix(self.test_webhook())
-		try:
-			self.assertEqual(type(btrx.btrx), type(test_btrx_var))
-		except:
-			raise TypeError(
-				f"Type of btrx = <class 'fastbtrx24.bitrtix.Bitrix'> but now:\nbtrx: {type(btrx.btrx)} != test_btrx: {type(test_btrx_var)}"
-			)
-		return test_btrx_var
-
-	def test_datalist(self):
-		self.datalist = [{"key": 'data'}]
-		self.assertEqual(type(datalist), type(self.datalist))
-
-	def test_filename(self):
-		self.filename = 'file.json'
-		self.assertEqual(type(filename), type(self.filename))
-		if type(self.filename) != str:
-			raise TypeError('type of filename = str')
-
-	def test_path(self):
-		self.path = 'tests\\btrx_test'
-		self.assertEqual(type(path), type(self.path))
-		if type(self.path) != str:
-			raise TypeError('type of path = str')
+	def test_getproduct_list(self):
+		self.datalist = []
+		self.assertEqual(type(Btrx.get_product_list(self.datalist)),type(self.datalist))
 
 	def test_save_to_json(self):
-		self.datalist = [{"key": 'data'}]
-		self.filename = 'file.json'
-		self.path = f'{os.getcwd()}\\tests\\btrx_test'
+		Btrx.save_to_json()
+		self.datalist = []
+		self.assertEqual(type([]),type(self.datalist))
 
+	# def test_get_product_list(self):
+	# 	self.assertEqual(type(get_product_list(self.test_btrx())), list)
 
-		self.assertEqual(type(datalist), type(self.datalist))
-		self.assertEqual(type(filename), type(self.filename))
-		self.assertEqual(type(path), type(self.path))
-
-
-	def test_load_from_jsonFile(self):
-		self.filename = 'file.json'
-		self.path = f'{os.getcwd()}\\tests\\btrx_test'
-		self.data_from_file = []
-		data_from_file = []
-		self.assertEqual(type(data_from_file), list)
-		self.assertEqual(type(self.data_from_file), list)
-		os.chdir(self.path)
-		self.assertEqual(type(load_from_jsonFile(self.filename, self.path)),list)
-		self.assertEqual(load_from_jsonFile(self.filename, self.path),list)
-		with self.assertRaises(Exception) as e:
-			load_from_jsonFile(self.filename, 2)
-		self.assertEqual('тип path должен быть str', e.exception.args[0])
-		with self.assertRaises(Exception) as e:
-			load_from_jsonFile(2, self.path)
-		self.assertEqual('тип filename должен быть str', e.exception.args[0])
-
-	def test_get_product_list(self):
-		self.assertEqual(type(get_product_list(self.test_btrx())), list)
-
-	def test_get_all_data(self):
-		self.assertEqual(type(get_product_list(self.test_btrx())), list)
+	# def test_get_all_data(self):
+	# 	self.assertEqual(type(get_product_list(self.test_btrx())), list)
 
 
 if __name__ == "__main__":
