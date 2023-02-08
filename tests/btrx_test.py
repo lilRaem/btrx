c15e04bee8f1736d619f6ccd5dec5785d6db2563
raemi <maxim3053@gmail.com>
2023-02-08 23:25:25 +0300

    Add and update tests

--- tests/btrx_test.py
+++ tests/btrx_test.py
@@ -4,15 +4,15 @@ from module.btrx import Btrx
 
 
 class BtrxTest(TestCase):
-
+	path = os.getcwd()
 	filename = "file.json"
 	datalist = []
 
 	def test_save_to_json(self):
-		self.assertEqual(type(Btrx.save_to_json(self.datalist,self.filename,f"{os.getcwd()}\\tests\\btrx_save_test")),type(self.datalist))
+		self.assertEqual(type(Btrx.save_to_json(self,self.datalist,self.filename,f"{os.getcwd()}\\tests\\btrx_save_test")),type(self.datalist))
 
 	def test_load_from_jsonFile(self):
-		self.assertEqual(type(Btrx.load_from_jsonFile(self,self.filename,f"{os.getcwd()}\\tests\\btrx_load_test\\")),type(self.datalist))
+		self.assertEqual(type(Btrx.load_from_jsonFile(self,self.filename,f"tests\\btrx_load_test")),type(self.datalist))
 
 	def test_getproduct_list(self):
 		self.assertEqual(type(Btrx.get_product_list(self)),type(self.datalist))
@@ -24,8 +24,12 @@ class BtrxTest(TestCase):
 		self.assertEqual(type(Btrx.get_product_list(self.datalist)), type(self.datalist))
 
 	def test_check_product(self):
+		self.assertEqual(type(Btrx.check_product(self,"Ультразвуковая диагностика", "-6400",Btrx.get_all_data(self, Btrx.get_product_list(Btrx())))), type(self.datalist))
+		self.assertEqual(type(Btrx.check_product(self,"Ультразвуковая диагностика", "-0",Btrx.get_all_data(self, Btrx.get_product_list(Btrx())))), type(self.datalist))
+		self.assertEqual(type(Btrx.check_product(self,"Ультразвуковая диагностика", "0",Btrx.get_all_data(self, Btrx.get_product_list(Btrx())))), type(self.datalist))
+		self.assertEqual(type(Btrx.check_product(self,"Ультразвуковая диагностика", "39",Btrx.get_all_data(self, Btrx.get_product_list(Btrx())))), type(self.datalist))
+		self.assertEqual(type(Btrx.check_product(self,"Ультразвуковая диагностика", "6400",Btrx.get_all_data(self, Btrx.get_product_list(Btrx())))), type(self.datalist))
+		self.assertEqual(type(Btrx.check_product(self,"Ультразвуковая диагностика", "99000",Btrx.get_all_data(self, Btrx.get_product_list(Btrx())))), type(self.datalist))
+		self.assertEqual(type(Btrx.check_product(self,"Онкология", "0",Btrx.get_all_data(self, Btrx.get_product_list(Btrx())))), type(self.datalist))
+		self.assertEqual(type(Btrx.check_product(self,"Онкология", "39",Btrx.get_all_data(self, Btrx.get_product_list(Btrx())))), type(self.datalist))
 		self.assertEqual(type(Btrx.check_product(self,"Онкология", "6400",Btrx.get_all_data(self, Btrx.get_product_list(Btrx())))), type(self.datalist))
-
-if __name__ == "__main__":
-	test = BtrxTest()
-	test.test_check_product()
