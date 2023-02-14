784524e579fa0afef118fe2823b9633aaa7e96c7
raemi <maxim3053@gmail.com>
2023-02-14 10:38:41 +0300

    improve search

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from time import time
 import json
 from module.config import FinalData, makefileWdateName
 from colorama import Fore, Back, Style
-from datetime import date
 from module.btrx import Btrx
 # from module.build_btrx_data import buildjsondata
 from module.parsersearchsite import searchInSite, getProgramUrl
@@ -15,8 +14,9 @@ from module.parsersearchsite import searchInSite, getProgramUrl
 
 datalist = []
 
-
-def main(search_name='Онкология', search_price='0'):
+clear = lambda: os.system('cls')
+def main(search_name='Ультразвуковая диагностика', search_price='99000'):
+	clear()
 	fdata = []
 	start = time()
 	btrx = Btrx()
@@ -54,11 +54,15 @@ def main(search_name='Онкология', search_price='0'):
 				final_data.linkNmo = _final_data['linkNmo']
 				if count_getProgramUrl == 1:
 					final_data.url = progUrl_data[0]['url']
+					if _final_data['hour'] == None:
+						final_data.hour = progUrl_data[0]['hour']
 				else:
 					for d in progUrl_data:
 						if final_data.name.lower() == d['name'].replace("Курс ","").lower():
 							if final_data.price == d['price']:
 								final_data.url = d['url']
+							if final_data.hour == None:
+								final_data.hour = d['hour']
 			print("\n" + Fore.GREEN + f'{final_data.json(encoder="utf-8",ensure_ascii=False)}')
 			fdata.append(final_data.json(encoder="utf-8",ensure_ascii=False))
 		else:
@@ -70,27 +74,33 @@ def main(search_name='Онкология', search_price='0'):
 					final_data.price = json_check_data[iter_data]['price']
 					final_data.linkNmo = json_check_data[iter_data]['linkNmo']
 					progUrl_data = getProgramUrl(final_data.name, final_data.price)
-					json_progUrl_data = progUrl_data
-					for i,v  in enumerate(json_progUrl_data):
-						if v['price']:
-							final_data.url = v['url']
 					try:
 						final_data.hour = json_check_data[iter_data]['hour']
 					except:
-						final_data.hour = json_progUrl_data['hour']
+						final_data.hour = progUrl_data['hour']
+					for i,v  in enumerate(progUrl_data):
+						if v['price']:
+							final_data.url = v['url']
+						if v['hour'] == final_data.hour:
+							pass
+						else:
+							if final_data.hour == None:
+								final_data.hour = v['hour']
 					print("\n" + Fore.GREEN + f'{final_data.json(encoder="utf-8",ensure_ascii=False)}' + Fore.RESET)
 					fdata.append(final_data.json(encoder="utf-8",ensure_ascii=False))
 		if fdata:
 			for v in fdata:
 				vv = json.loads(v)
 				if vv['price'] == search_price:
-					print(Back.WHITE+Fore.LIGHTGREEN_EX+f"\n*****\nid: {vv['id']}\nname: {vv['name']}\nprice: {vv['price']}\nhour: {vv['hour']}\n{vv['url']}"+Fore.RESET+Back.RESET)
+					print(Fore.WHITE+f"\n{Back.GREEN}*****\n{Style.DIM}id: {vv['id']}\nname: {vv['name']}\nprice: {vv['price']}\nhour: {vv['hour']}\n{vv['url']}"+Fore.RESET+Back.RESET)
+					print(Fore.CYAN+f"\n{vv['url']}?program={vv['name']}&header=Курс {vv['name']}&cost={vv['price']}&tovar={vv['id']}&sendsay_email="+"${ Recipient.Email }"+Fore.RESET)
 				else:
-					print(Fore.LIGHTGREEN_EX+f"\n*****\nid: {vv['id']}\nname: {vv['name']}\nprice: {vv['price']}\nhour: {vv['hour']}\n{vv['url']}"+Fore.RESET)
+					print(Fore.WHITE+f"\n{Back.LIGHTGREEN_EX}*****\n{Style.DIM}id: {vv['id']}\nname: {vv['name']}\nprice: {vv['price']}\nhour: {vv['hour']}\n{vv['url']}"+Fore.RESET+Back.RESET)
+					print(Fore.CYAN+f"\n{vv['url']}?program={vv['name']}&header=Курс {vv['name']}&cost={vv['price']}&tovar={vv['id']}&sendsay_email="+"${ Recipient.Email }"+Fore.RESET)
 	else:
 		btrx.save_to_json(btrx.get_product_list(), makefileWdateName(path)[1], path)
 		data = btrx.load_from_jsonFile(makefileWdateName(path)[1],path)
-		btrx.check_product(search_name, final_data.price, btrx.get_all_data(data))
+		btrx.check_product(search_name, search_price, btrx.get_all_data(data))
 	print('\n'+Fore.MAGENTA+f"(main.py) Search time: {round(time()-start,2)} sec"+ Fore.RESET)
 
 if __name__ == "__main__":
