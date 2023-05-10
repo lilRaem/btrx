3ef86f94e401701fdc372de940cc9a3439c00c30
raemi <maxim3053@gmail.com>
2023-05-11 01:34:22 +0300

    add selenium driver

---
+++ module/template_generator/config_generator/webdriver_config.py
@@ -0,0 +1,32 @@
+from random import choice
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from os import environ
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+user_agent_list = [
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+]
+user_agent = choice(user_agent_list)
+
+
+environ['WDM_LOG_LEVEL'] = '0'
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+chrome_options.add_experimental_option("detach", True)
+chrome_options.add_argument("--log-level=OFF")
+# chrome_options.add_argument('--headless')
+chrome_options.add_argument(f"user-agent={user_agent}")
+capabilities = DesiredCapabilities.CHROME
+capabilities['goog:loggingPrefs'] = {'browser': 'ALL',
+                           'driver': 'ALL'}
+
+def selenium_start():
+    return webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options,desired_capabilities=capabilities)
